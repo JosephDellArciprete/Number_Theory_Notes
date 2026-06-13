e66c3446f9d89fab123b956aa06dd69c15a024ba
Joseph Dell'Arciprete <158191787+JosephDellArciprete@users.noreply.github.com>
2026-06-13 02:07:32 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,34 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Sets",
-  "body": " Sets  Filler text lol.  "
+  "body": " Sets   Sets and Subsets  Sets are determined uniquely by their elements. The primitive, undefined concept in set theory is that of membership .    A set is a collection of objects, called the elements of the set. To denote an element is a member of a set we write . If is not an element of we write . By definition a set is totally determined by its elements, so two sets are equal if and only if they have the same elements: .    As mentioned above the key concept is that of membership, we do not care about repeats of element nor of the order of the elements. The set of US presidents between 1789 to 2026 is the same if we list the in ascending order, descending order, or if we repeat some of the presidents in our description of the set  We immediately come to another simple concept for out next definition.    If A and B are sets we say that A is a subset of B, written , if every element of A is also an element of B, that is:     We must not abuse the the concepts of set membership too much though, consider Russel's Paradox , named after logician Bertrand Russell.   Russell's Paradox  Let be the set of all sets such that . Is ? One the one hand if then by definition, . However if again by definition . So in either case we get a contradiction. Hence R is not a well defined set.   One way to avoid such problems is to have a well defined universal set and define a subsets of this set by defining a valid property that defines a subset. Particular for a set and a predicate  on , i.e a valid statement that is true of false, then: Is a subset on , this is known as predicate form .  Another easy to define a set is simply to list its elements, straightforward but obviously not often feasible.   On the other hand we also have the empty set , denoted , the set containing no elements.        Set Operations  filler    Algebra of Sets  fillers    Generalized Operations  filler lol    Product Sets  pootis   "
+},
+{
+  "id": "def-Set",
+  "level": "2",
+  "url": "sets.html#def-Set",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  A set is a collection of objects, called the elements of the set. To denote an element is a member of a set we write . If is not an element of we write . By definition a set is totally determined by its elements, so two sets are equal if and only if they have the same elements: .   "
+},
+{
+  "id": "def-Subset",
+  "level": "2",
+  "url": "sets.html#def-Subset",
+  "type": "Definition",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  If A and B are sets we say that A is a subset of B, written , if every element of A is also an element of B, that is:    "
+},
+{
+  "id": "subsec_sets_and_subsets-8",
+  "level": "2",
+  "url": "sets.html#subsec_sets_and_subsets-8",
+  "type": "Warning",
+  "number": "1.1.3",
+  "title": "Russell’s Paradox.",
+  "body": " Russell's Paradox  Let be the set of all sets such that . Is ? One the one hand if then by definition, . However if again by definition . So in either case we get a contradiction. Hence R is not a well defined set.  "
 },
 {
   "id": "relations",
